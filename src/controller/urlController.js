d2c4018eca3396ab301c3e8b70c8394bbaef8baf
Swapnali <sonawanesn210@gmail.com>
2022-05-18 15:35:10 +0530

    redis added

--- src/controller/urlController.js
+++ src/controller/urlController.js
@@ -1,6 +1,32 @@
 const urlModel=require('../model/urlModel')
 const validUrl=require('valid-url')
 const shortid = require('shortid');
+const redis=require('redis')
+const { promisify } = require("util");
+
+
+//========================Connect to redis===============================================================//
+const redisClient = redis.createClient(
+  13190,
+  "redis-13190.c301.ap-south-1-1.ec2.cloud.redislabs.com",
+  { no_ready_check: true }
+);
+redisClient.auth("gkiOIPkytPI3ADi14jHMSWkZEo2J5TDG", function (err) {
+  if (err) throw err;
+});
+
+redisClient.on("connect", async function () {
+  console.log("Connected to Redis..");
+});
+
+//Connection setup for redis
+
+const SET_ASYNC = promisify(redisClient.SET).bind(redisClient);
+const GET_ASYNC = promisify(redisClient.GET).bind(redisClient);
+
+
+//==============================================POST /url/shorten================================//
+
 
 const createUrl=async (req,res) =>{
 try{
@@ -24,7 +50,7 @@ if (!validUrl.isUri(baseUrl)) {
 }
 
 if (!validUrl.isUri(longUrl)) { return res.status(400).send({ status: false, message: 'Please provide a valid longurl' }) }
-
+//if (!validUrl.isWebUri(longUrl)) { return res.status(400).send({ status: false, message: 'Please provide a valid URL' }) }
 let uniqueLongUrl=await urlModel.findOne({longUrl})
 if(uniqueLongUrl){
     return res.status(400).send({ status: false, message: `${longUrl} this urlcode  Already exist.Please,try again with another url` })
